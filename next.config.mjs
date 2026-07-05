f04a3d96e585f4295d8e79117747ae2a3057c76f
imthiranu <imthiranu@gmail.com>
2026-07-05 11:10:45 +0530

    Configure build to ignore eslint/typescript checks during vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
       bodySizeLimit: "5mb",
     },
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
